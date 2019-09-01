024129f704d62fedb70ed215cf047811f221265a
Dilyana Mihaylova <dilyana.p.mihaylova@gmail.com>
2019-09-01 17:20:25 +0300

    add form validation

--- src/components/contact/Form.tsx
+++ src/components/contact/Form.tsx
@@ -7,6 +7,7 @@ import Button from '@material-ui/core/Button';
 import ADD_CONTACT from '../../graphql/mutations/addContact';
 import UPDATE_CONTACT from '../../graphql/mutations/updateContact';
 import GET_CONTACTS from '../../graphql/queries/contactsList';
+import validateField from './../../utils/formValidation';
 import './Contact.css';
 
 class Form extends Component<{
@@ -22,38 +23,60 @@ class Form extends Component<{
     email: this.props.email,
     mutation: this.props.mutation,
     title: this.props.title,
-    redirect: false
+    redirect: false,
+    nameValid: this.props.mutation === 'ADD_CONTACT' ? false : true,
+    emailValid: this.props.mutation === 'ADD_CONTACT' ? false : true
+  }
+
+  setFieldValue = (e: any) => {
+    const { name, value } = e.target;
+    if (name === 'name') {
+      this.setState({
+        name: value,
+        nameValid: validateField(name, value)
+      });
+    } else {
+      this.setState({
+        email: value,
+        emailValid: validateField(name, value)
+      });
+    }
   }
 
   render() {
-    const { id, name, email, mutation, redirect, title } = this.state;
+    const {
+      id,
+      name,
+      email,
+      mutation,
+      redirect,
+      title,
+      nameValid,
+      emailValid
+    } = this.state;
     return (
       <div className="contact-form">
         {!!redirect && <Redirect to="/contacts" />}
         <Typography variant="h4" className="form-title">{title}</Typography>
         <TextField
           required
-          id="name"
+          name="name"
           className="form-input"
           label="Name"
           value={name}
           margin="normal"
           variant="outlined"
-          onChange={e => this.setState({
-            name: e.target.value
-          })}
+          onChange={this.setFieldValue}
         />
         <TextField
           required
-          id="email"
+          name="email"
           className="form-input"
           label="Email"
           value={email}
           margin="normal"
           variant="outlined"
-          onChange={e => this.setState({
-            email: e.target.value
-          })}
+          onChange={this.setFieldValue}
         />
         <Mutation
           mutation={mutation === 'ADD_CONTACT' ? ADD_CONTACT : UPDATE_CONTACT}
@@ -73,7 +96,7 @@ class Form extends Component<{
             return (
               <Button
                 onClick={submit}
-                disabled={name === '' || email === ''}
+                disabled={!nameValid || !emailValid}
                 variant="contained"
                 color="secondary"
                 size="large"
---
+++ src/utils/formValidation.tsx
@@ -0,0 +1,15 @@
+const validateField = (fieldName: string, value: string) => {
+  switch(fieldName) {
+    case 'name':
+      const nameValid = value.length >= 3;
+      return nameValid;
+    case 'email':
+      const regExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      const emailValid = regExp.test(value);
+      return emailValid;
+    default:
+      break;
+  }
+}
+
+export default validateField;
